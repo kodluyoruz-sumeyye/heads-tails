5caf60368f0dd4318e9a2d07f3c932bf4ca0ae67
Sumeyye Uzgor <summeyyeoz@gmail.com>
2021-06-18 17:56:04 +0300

    project is completed

--- src/components/CoinFlipper/CoinFlipper.js
+++ src/components/CoinFlipper/CoinFlipper.js
@@ -10,13 +10,24 @@ class CoinFlipper extends Component {
     this.state = {
       side: "tura",
       flipping: false,
+      total: 0,
+      heads: 0,
+      tails: 0,
     };
   }
   handleClick = () => {
     // "At!" butonuna tıkladığımızda paranın dönmesini istiyoruz, bu yüzden "flipping" durumunu "true" yapıyoruz.
-    this.setState({ flipping: true });
+    let rndNum = Math.random();
+    this.setState({
+      ...this.state,
+      flipping: true,
+      total: this.state.total + 1,
+      side: rndNum < 0.5 ? "tura" : "yazi",
+      heads: this.state.heads + (rndNum < 0.5 ? 1 : 0),
+      tails: this.state.tails + (rndNum < 0.5 ? 0 : 1),
+    });
     // 1 saniye kadar dönmesi yeterli, bu yüzden 1 saniye sonra "flipping" durmunu tekrar "false" yapıyoruz.
-    setTimeout(() => this.setState({ flipping: false }), 1000);
+    setTimeout(() => this.setState({ ...this.state, flipping: false }), 1000);
   };
 
   render() {
@@ -27,10 +38,10 @@ class CoinFlipper extends Component {
         <button onClick={this.handleClick}>At!</button>
         <p>
           Toplam
-          <strong> 5 </strong>
+          <strong> {this.state.total} </strong>
           atıştan
-          <strong> 3 </strong>ü tura
-          <strong> 2 </strong>
+          <strong> {this.state.heads} </strong>ü tura
+          <strong> {this.state.tails} </strong>
           si yazı geldi.
         </p>
       </div>
